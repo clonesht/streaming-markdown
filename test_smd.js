ff40955f09659e0a00e929b95255c5b898590ec1
Clonesht <62205813+clonesht@users.noreply.github.com>
2025-11-03 21:36:43 +0100

    Fix formatting underscores in word

---
+++ test_smd.js
@@ -0,0 +1,18 @@
+import * as smd from "./smd.js"
+
+let renderer = smd.logger_renderer();
+let parser = smd.parser(renderer);
+smd.parser_write(parser, "a_this_")
+smd.parser_end(parser)
+
+console.log("---")
+
+parser = smd.parser(renderer);
+smd.parser_write(parser, "but\n_this_")
+smd.parser_end(parser)
+
+console.log("---")
+
+parser = smd.parser(renderer);
+smd.parser_write(parser, "_this_")
+smd.parser_end(parser)
